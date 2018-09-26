d55de5e043374ce68a5cb4b68838ca5c70e61be4
nmgarton <nmgarton@isu-csafe.stat.iastate.edu>
2018-09-26 15:52:16 -0500

    Documentation for the the wrapper function that runs both of the one changepoint analyses (left and right).

--- R/changepoint_gibbs_1cp_v2.R
+++ R/changepoint_gibbs_1cp_v2.R
@@ -1,3 +1,34 @@
+#'  Estimate a posterior distribution of data conditional that there is one groove.
+#'
+#' This function is basically a wrapper for running the left and right (one) changepoint
+#' Gibbs algorithms. The only computation that this function does is to estimate the posterior
+#' means of the left and right changepoint distributions.
+#' @param data Data frame with columns "x" and "y." "x" is a column of the locations of the
+#' observed residual values, y.
+#' @param iter Number of interations after warmup.
+#' @param start.vals.left Starting values for the changepoint algorithm assuming the groove is on the left.
+#' List with elements "sigma", "l", "cp", "beta", and "intercept." "sigma" and "l"
+#'   are 2 element vectors where the first element is for the data to the left of the changepoint.
+#'   "cp" is the changepoint starting value. "beta" and "intercept" are the slope and intercept
+#'   starting values for the mean of the data model to the left of the changepoint.
+#'  which parameterize the covariance matrix.
+#' @param start.vals.right Starting values for the changepoint algorithm assuming the groove is on the right.
+#' @param prop_var_left The proposal variance for the random walk Metropolis algorithm assuming
+#' that the groove is on the left.
+#' A two element list of the proposal variance-covariance matrices for the random
+#' walk metropolis algorithm(s). The first element is for the data to the left of the changepoint.
+#' @param prop_var_right The proposal variance for the random walk Metropolis algorithm assuming
+#' that the groove is on the right.
+#' @param cp_prop_var The proposal variance for the changepoint.
+#' @param tol This parameter controls how close changepoint proposals can be to the edge of the data
+#' before getting automatically rejected. For example, a value of 10 means that the changepoint will be
+#' automatically rejected if the proposal is within a distance of 10 x-values from either edge.
+#' @param warmup The number of initial iterations which serves two purposes: the first is to allow the
+#' algorithm to wander to the area of most mass, and the second is to tune the proposal variance.
+#' @param verbose Logical value indicating whether to print the iteration number and the parameter proposals.
+#' @return A named list with all of the output that the left and right changepoint functions produce
+#' individually plus the posterior means of the left and right changepoints.
+#' @export
 cp1_gibbs_v2 <- function(data, iter, start.vals.left, start.vals.right, prop_var_left, prop_var_right, cp_prop_var, tol = 10, warmup = 5000, verbose = FALSE)
 {
   ##data is a data frame with column x and column y
@@ -12,8 +43,8 @@ cp1_gibbs_v2 <- function(data, iter, start.vals.left, start.vals.right, prop_var
   # prior <- 0.5
   # pleft <- (prior * mean(left_cp_out$lp)) / (prior * mean(left_cp_out$lp) + (1-prior) * mean(right_cp_out$lp))
   # pright <- 1 - pleft
-  pleft <- NA
-  pright <- NA
+  # pleft <- NA
+  # pright <- NA
 
   ## estimated changepoint
   est_left_cp <- mean(left_cp_out$parameters$cp)
@@ -22,7 +53,7 @@ cp1_gibbs_v2 <- function(data, iter, start.vals.left, start.vals.right, prop_var
   return(list("left_parameters" = left_cp_out$parameters, "right_parameters" = right_cp_out$parameters,
               "accept" = list("left" = left_cp_out$accept, "right" = right_cp_out$accept),
               "lp" = list("left" = left_cp_out$lp, "right" = right_cp_out$lp),
-              "ppleft" = pleft, "ppright" = pright, "estimated_cp" = list("left" = est_left_cp, "right" = est_right_cp))
+              "estimated_cp" = list("left" = est_left_cp, "right" = est_right_cp))
          )
 }
 
