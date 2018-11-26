1ab0e0c3229a381006f790e3a730a5c585be8fe9
Nate Garton <nate.garton13@gmail.com>
2018-11-26 13:14:13 -0600

    Document data. Need a source for the data set.

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Example of an average of 2D crosscuts from the Hamby 44 data set.
+#'
+#' This data set is essentially a 2D crosscut from a land in the Hamby 44 set of bullet land scans.
+#'
+#' @format A data frame with 3346 rows and two variables:
+#' \describe{
+#'     \item{x}{location variable}
+#'     \item{value}{the height of the land at the given location described by x}
+#' }
+#' @source Hamby 44
+"raw_data"
