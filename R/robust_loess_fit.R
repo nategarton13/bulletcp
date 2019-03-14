e0ef3771c498306d98b0da786ca0d326a536ced3
Nate Garton <nate.garton13@gmail.com>
2019-03-14 11:02:17 -0500

    Add example to robust LOESS.

--- R/robust_loess_fit.R
+++ R/robust_loess_fit.R
@@ -6,6 +6,24 @@
 #' @importFrom stats loess
 #' @importFrom assertthat assert_that
 #' @importFrom assertthat has_name
+#' @example
+#' data("example_data")
+#' head(raw_data)
+#' raw_data <- raw_data[seq(from = 1, to = nrow(raw_data), by = 30),]
+#' plot(raw_data$x, raw_data$y)
+#'
+#' # set the minimum y-value to zero
+#' check_min <- min(raw_data$value[!is.na(raw_data$value)])
+#' raw_data <- dplyr::mutate(raw_data, value_std = value - check_min)
+#'
+#' # remove global structure
+#' rlo_fit <- robust_loess_fit(cc = raw_data, iter = 20)
+#' raw_data$rlo_pred <- predict(rlo_fit, newdata = raw_data)
+#' raw_data$rlo_resid <- raw_data$value_std - raw_data$rlo_pred
+#'
+#' # define new data frame without the global structure
+#' data <- data.frame("x" = raw_data$x, "y" = raw_data$rlo_resid)
+#' plot(data$x, data$y)
 #' @export
 robust_loess_fit <- function(cc, iter) {
   assert_that(has_name(cc, "x"), has_name(cc, "value_std"))
