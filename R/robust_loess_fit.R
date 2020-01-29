b6a3d6f84ddf008c4d3ef7988d0e28bfe646d7dd
Nate Garton <nate.garton13@gmail.com>
2020-01-29 18:35:13 -0500

    make raw_data example_data

--- R/robust_loess_fit.R
+++ R/robust_loess_fit.R
@@ -8,21 +8,21 @@
 #' @importFrom assertthat has_name
 #' @examples
 #' data("example_data")
-#' head(raw_data)
-#' raw_data <- raw_data[seq(from = 1, to = nrow(raw_data), by = 30),]
-#' plot(raw_data$x, raw_data$y)
+#' head(example_data)
+#' example_data <- example_data[seq(from = 1, to = nrow(example_data), by = 30),]
+#' plot(example_data$x, example_data$y)
 #'
 #' # set the minimum y-value to zero
-#' check_min <- min(raw_data$value[!is.na(raw_data$value)])
-#' raw_data <- dplyr::mutate(raw_data, value_std = value - check_min)
+#' check_min <- min(example_data$value[!is.na(example_data$value)])
+#' example_data <- dplyr::mutate(example_data, value_std = value - check_min)
 #'
 #' # remove global structure
-#' rlo_fit <- robust_loess_fit(cc = raw_data, iter = 20)
-#' raw_data$rlo_pred <- predict(rlo_fit, newdata = raw_data)
-#' raw_data$rlo_resid <- raw_data$value_std - raw_data$rlo_pred
+#' rlo_fit <- robust_loess_fit(cc = example_data, iter = 20)
+#' example_data$rlo_pred <- predict(rlo_fit, newdata = example_data)
+#' example_data$rlo_resid <- example_data$value_std - example_data$rlo_pred
 #'
 #' # define new data frame without the global structure
-#' data <- data.frame("x" = raw_data$x, "y" = raw_data$rlo_resid)
+#' data <- data.frame("x" = example_data$x, "y" = example_data$rlo_resid)
 #' plot(data$x, data$y)
 #' @export
 robust_loess_fit <- function(cc, iter) {
